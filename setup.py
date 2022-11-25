1f4306566f5c9fac15af9841f7af3bc8dd38a235
tobirohrer <tobias.rohrer@outlook.com>
2022-11-25 16:53:34 +0100

    adding csv files to package data

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from distutils.core import setup
 
 setup(name='building_energy_storage_simulation',
-      version='0.1',
+      version='0.4',
       description='A simulation of a building to optimize energy storage utilization.',
       author='Tobias Rohrer',
       author_email='tobias.rohrer@outlook.com',
+      # Required to include profiles which are stored as .csv files
+
       # Use `packages` argument to tell distutils where the python modules are located.
       # See https://docs.python.org/3/distutils/setupscript.html#listing-whole-packages for more details.
       packages=['building_energy_storage_simulation'],
+      package_dir={'building_energy_storage_simulation': 'building_energy_storage_simulation'},
+      package_data={
+          "building_energy_storage_simulation": [
+              "data/preprocessed/*.csv",
+          ]
+      },
+      include_package_data=True,
       # Use `install_requires` to specify the dependencies which are required in order to run the package.
       # See https://packaging.python.org/en/latest/discussions/install-requires-vs-requirements/ for a differentiation
       # to the `requirements.txt`.
