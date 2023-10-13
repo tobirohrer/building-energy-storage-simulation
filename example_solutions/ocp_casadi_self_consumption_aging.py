7c1122874e3c1c3ebb6a27821253befa6b11bf6d
Hogl <ric57834@ise.fhg.de>
2023-10-13 10:05:01 +0200

    included simple battery aging in selfconsumption example

--- example_solutions/ocp_casadi_self_consumption_aging.py
+++ example_solutions/ocp_casadi_self_consumption_aging.py
@@ -6,12 +6,15 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 DELTA_TIME_HOURS = 1
+HOURS_PER_YEAR = 24*365
+energy_capacity = 200  # kWh
+replacement_price = energy_capacity * 100  # kWh*€/kWh
 
 
 def read_data():
     load = pd.read_csv('../building_energy_storage_simulation/data/preprocessed/electricity_load_profile.csv')[
         'Load [kWh]']
-    price = 0.2
+    price = 0.4
     generation = pd.read_csv('../building_energy_storage_simulation/data/preprocessed/solar_generation_profile.csv')[
         'Generation [kWh]']
     return np.array(load), np.array(price), np.array(generation)
@@ -25,8 +28,13 @@ def build_optimization_problem(residual_fixed_load, price, soc=0):
     max_power_discharge = -1 * max_power_charge
     max_soc = 1
     min_soc = 0
+    min_soh = 0.8
+    max_soh = 1
+    max_life_time = 15 #years
+    max_life_time_hours = max_life_time*HOURS_PER_YEAR
+    max_cycles = 10000
     energy_capacity = 200  # kWh
-
+    replacement_price = energy_capacity * 100 #kWh*€/kWh
     w = []  # list of control variables
     w0 = []  # initial guess for state variables
     lbw = []  # lower bound for state variables
@@ -35,16 +43,23 @@ def build_optimization_problem(residual_fixed_load, price, soc=0):
     g = []  # list of constraints
     lbg = []  # lower bound for constraints
     ubg = []  # upper bound for constraints
-    soc_k = casadi.SX.sym("soc0")  # define first state variable as casadi symbol
+    soc_k = casadi.SX.sym("soc_0")  # define first state variable as casadi symbol
     w += [soc_k]  # add first control variable into state vector
     lbw += [soc_init]  # add state bounds, in this case start with initial value
     ubw += [
         soc_init]  # also the upper one, which is also the same as the lower bound since this is an equality constraint
     w0 += [soc_init]  # use initial value for this
+    soh_k = casadi.SX.sym("soh_0")
+    w += [soh_k]
+    lbw += [1.0]
+    ubw += [1.0]
+    w0 += [1.0]
+    # d_sohs_calendrical = []
+    # d_sohs_cyclical = []
     print(len(residual_fixed_load))
     for k in range(len(residual_fixed_load)):
         battery_charge_power_k = casadi.SX.sym("power_" + str(k))  # casadi symbol for control variable
-        w += [battery_charge_power_k]  # why is this also in w vector - is this combined for state and control?
+        w += [battery_charge_power_k]
         lbw += [max_power_discharge]
         ubw += [max_power_charge]
         w0 += [0]  # use 0 power as initial guess
@@ -54,17 +69,29 @@ def build_optimization_problem(residual_fixed_load, price, soc=0):
         ubw += [math.inf]
         w0 += [residual_fixed_load[k]]
         # compute intermediate result of soc update formula
-        soc_end = soc_k + DELTA_TIME_HOURS * battery_charge_power_k / energy_capacity
+        d_soc = DELTA_TIME_HOURS * battery_charge_power_k / energy_capacity
+        soc_end = soc_k + d_soc
         soc_k = casadi.SX.sym(f"soc_end{k + 1}")
-        J = J + price * np.log(1 + np.exp(grid_power_k))  # minimize grid power
         w += [soc_k]
         lbw += [min_soc]
         ubw += [max_soc]
         w0 += [0.5]
-
-        g += [soc_end - soc_k, grid_power_k - (residual_fixed_load[k] + battery_charge_power_k)]
-        lbg += [0, 0]
-        ubg += [0, 0]
+        d_soh_calendrical = 0.2/max_life_time_hours
+        d_soc_charging = np.log(1 + np.exp(d_soc))
+        d_soc_discharging = np.log(1 + np.exp(-d_soc))
+        d_soh_cyclical_charging = 0.2 * (d_soc_charging/2)/max_cycles
+        d_soh_cyclical_discharging = 0.2 * (d_soc_discharging/2)/max_cycles
+        d_soh = d_soh_calendrical + d_soh_cyclical_charging + d_soh_cyclical_discharging
+        soh_end = soh_k - d_soh
+        soh_k = casadi.SX.sym(f"soh_end{k+1}")
+        w+= [soh_k]
+        lbw += [min_soh]
+        ubw += [max_soh]
+        w0 += [1.0]
+        J = J + price * np.log(1 + np.exp(grid_power_k)) + replacement_price * d_soh * 5 # minimize grid power
+        g += [soc_end - soc_k, soh_end-soh_k, grid_power_k - (residual_fixed_load[k] + battery_charge_power_k)]
+        lbg += [0, 0, 0]
+        ubg += [0, 0, 0]
 
     prob = {"f": J, "x": casadi.vertcat(*w), "g": casadi.vertcat(*g)}
     print(J.shape, len(w), len(g), len(w0))
@@ -78,7 +105,6 @@ if __name__ == "__main__":
     load, price, generation = read_data()
 
     load = load[0:8700]
-    # price = price[0:8700]
     generation = generation[0:8700]
 
     residual_fixed_load = load - generation
@@ -86,26 +112,27 @@ if __name__ == "__main__":
 
     traj = build_optimization_problem(residual_fixed_load, price, soc=0)
     t = [time[i] * DELTA_TIME_HOURS for i in time]
-    soc_traj = traj[0::3]
-    battery_charge_power_traj = traj[1::3]
-    grid_power_traj = traj[2::3]
+    soc_traj = traj[0::4]
+    soh_traj = traj[1::4]
+    battery_charge_power_traj = traj[2::4]
+    grid_power_traj = traj[3::4]
     print(traj.shape)
-    baseline_cost = sum(residual_fixed_load[residual_fixed_load > 0] * price)
+    baseline_cost = sum(residual_fixed_load[residual_fixed_load > 0] * price) + (1/15*replacement_price)
     augmented_load = np.array([(battery_charge_power_traj[i]) for i in time]) + residual_fixed_load
-    cost = sum(grid_power_traj[grid_power_traj > 0]) * price
+    cost = sum(grid_power_traj[grid_power_traj > 0]) * price + replacement_price * (1-soh_traj[-1])*5
 
     print('baseline cost: ' + str(baseline_cost))
     print('cost: ' + str(cost))
     print('savings in %: ' + str(1 - cost / baseline_cost))
-
+    print('battery soh at end: '+str(soh_traj[-1]))
     fig1 = plt.figure()
     ax = plt.subplot()
     Line1 = ax.plot(time, [(residual_fixed_load[i]) for i in time], label='Residual Load')
-    Line2 = ax.plot(time, augmented_load, label='Augmented Load')
-    Line3 = ax.plot(time, [price] * 8700, '--', label='Price')
-    Line4 = ax.plot(time, [(battery_charge_power_traj[i]) for i in time], label='Battery Charge Power')
-    Line5 = ax.plot(time, [(grid_power_traj[i]) for i in time], label='Grid Power')
-    Line6 = ax.plot(time, [(soc_traj[i]) * 100 for i in time], label="Battery SOC")
+    Line2 = ax.plot(time, [price] * 8700, '--', label='Price')
+    Line3 = ax.plot(time, [(battery_charge_power_traj[i]) for i in time], label='Battery Charge Power')
+    Line4 = ax.plot(time, [(grid_power_traj[i]) for i in time], label='Grid Power')
+    Line5 = ax.plot(time, [(soc_traj[i]) * 100 for i in time], label="Battery SOC")
+    Line6 = ax.plot(time, [(soh_traj[i])*100 for i in time], label = "Battery SOH")
     plt.ylabel('Load and Battery Power Applied (kW) & Price (Cent per kWh)')
     plt.xlabel('Time Step')
     ax.legend()
